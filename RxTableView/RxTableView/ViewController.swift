c41f2e02463dad525f2c384abfb4481adeed7cab
ShivaniPR <shivani.pr@gmail.com>
2020-07-03 10:20:10 +0530

    Initial commit

---
+++ RxTableView/RxTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxTableView
+//
+//  Created by Shivani P R on 03/07/20.
+//  Copyright © 2020 Shivani P R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
